b1690a9b653b0a8e051db43e1e3c293a5d0e5d54
Ketevan Nanuashvili <100858778+KetiNanuashvili@users.noreply.github.com>
2023-04-09 01:44:46 +0400

    add sections

--- script.js
+++ script.js
@@ -133,3 +133,46 @@ setInterval( () =>{
 }, 4000)
 
 setSlide();
+
+//contact
+
+let RegistrationForm=document.getElementById('registration-form');
+
+RegistrationForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    let errors={
+
+    };
+    let formElement=event.target;
+
+
+
+    //username
+    let usernameValue = document.getElementById('username').value;
+
+    if (usernameValue==""){
+        errors.myusername="Username field can not be empty"
+    }
+    if (usernameValue.length <5){
+         errors.myusername="Username field can not be empty"
+    }
+});
+
+let emailField = document.getElementById('myemail');
+
+emailField.addEventListener('keydown', function(){
+    let emailValue =document.getElementById('myemail').value;
+    let text=document.getElementById('text');
+    let pattern=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (emailValue.match(pattern)){
+        emailField.style.border ="2px solid green";
+        text.innerText='Your email is valid';
+        text.style.color = 'green';
+
+    }else{
+        emailField.style.border ="2px solid red";
+        text.innerText='Your email is invalid';
+        text.style.color = 'red';
+    }
+});
